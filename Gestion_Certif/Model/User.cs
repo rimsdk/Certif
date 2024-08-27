b498d74144e7a601e840bd6450c6044d0acbabe1
Sadki Rim <128902889+rimsdk@users.noreply.github.com>
2024-08-28 00:46:21 +0200

    correction

--- Gestion_Certif/Model/User.cs
+++ Gestion_Certif/Model/User.cs
@@ -14,7 +14,7 @@ namespace Gestion_Certif.Model
         public string role { get; set; }
         public ICollection<Request_certif> SentRequests { get; set; } = new HashSet<Request_certif>();
         public ICollection<Request_certif> ReceivedRequests { get; set; } = new HashSet<Request_certif>(); public int? Request_certifId { get; set; }
-        public Request_certif? request { get; set; }
+
         public int? DepartementId { get; set; }
         public Departement? departement { get; set; }
         public string phoneNumer { get; set; }
