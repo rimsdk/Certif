43e0439d503f5eb56b0b5c89c4329594b8387c4d
Ayoub Bazhar <132767882+AyoubBazhar@users.noreply.github.com>
2024-08-30 16:21:24 +0100

    correct  the  conflict

--- Gestion_Certif/ViewModels/AddCertificateVM.cs
+++ Gestion_Certif/ViewModels/AddCertificateVM.cs
@@ -15,15 +15,10 @@ namespace Gestion_Certif.ViewModels
 
         public DateTime AchievementDate { get; set; }
             public int DepartementId { get; set; }
-<<<<<<< HEAD
+
             public int UserId { get; set; }
         public User? Sender { get; set; }
-       
-=======
-      
-        public int UserId { get; set; }
-        
->>>>>>> e198897e4e056c3dce016972bb2d359cef6e4999
+
 
 
 
